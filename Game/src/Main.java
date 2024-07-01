8f363e606a8b80ee48de9492d4f652d6ad6321b2
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2024-07-01 15:33:39 -0300

    for the commit

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -3,7 +3,7 @@ import Menus.MainMenu;
 public class Main {
     public static void main(String[] args) {
         boolean isPlaying = true;
-
+//      just for the commit
         while (isPlaying) {
             MainMenu mainMenu = new MainMenu();
             isPlaying = mainMenu.initialMenu();
